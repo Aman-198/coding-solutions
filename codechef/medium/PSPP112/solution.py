f932755ab80af676548436c8cebb7cfe05042db1
Aman Kumar <149381340+Aman-198@users.noreply.github.com>
2026-07-23 15:39:22 +0530

    ✅ PSPP112 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/medium/PSPP112/solution.py
@@ -0,0 +1,9 @@
+student = (["Alice", "Bob"], ["Math", "Science", "English"], 20, 'XYZ')
+
+# Update the code below this line
+list_1, list_2, num, string = student
+
+print(list_1)
+print(list_2)
+print(num)
+print(string[2])
