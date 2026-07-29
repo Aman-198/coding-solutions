7ca3019923ecfd9140db43f0cd0d92ebd6c9c5c3
Aman Kumar <149381340+Aman-198@users.noreply.github.com>
2026-07-29 13:40:21 +0530

    ✅ GS11 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/medium/GS11/solution.py
+++ codechef/medium/GS11/solution.py
@@ -6,6 +6,5 @@ for i in range(t):
     
     # accept 1 string on the 2nd line of each test case
     C = input()
-    
     # output the 2 integers and 1 string on a single line
     print(A, B, C)
